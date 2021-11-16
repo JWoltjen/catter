77a6c23e686c745f70f8ffcbbd7b2d052bebee6c
Jeff Woltjen (he/him) <Jeff.Woltjen@gmail.com>
2021-11-16 14:47:22 -0600

    style header component

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,16 @@
 import React from 'react'
+import {useSelector} from 'react-redux'
 
 function Header() {
+    const catState = useSelector(state => state.cats)
+    const starsTotal = catState.reduce((acc, catItem) => acc + catItem.upvotes, 0); 
+
     return (
         <header>
             <h1>Catter</h1>
-            <div>Total </div>
-            <div>Reset All</div>
+            <h2>Please enjoy some cats</h2>
+            <div>Total {starsTotal} ⭐</div>
+            <div>Reset All 🔄</div>
         </header>
     )
 }

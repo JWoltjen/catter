cdcefcdf5f726c4b441d92593500ac2e5a9381a1
Jeff Woltjen (he/him) <Jeff.Woltjen@gmail.com>
2021-11-16 14:38:20 -0600

    create header component

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <header>
+            <h1>Catter</h1>
+            <div>Total </div>
+            <div>Reset All</div>
+        </header>
+    )
+}
+
+export default Header
